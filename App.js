e56051c9d10950806764aca18e451d13ce066751
Ivan <IvanZaytsev123@gmail.com>
2020-10-20 11:48:07 -0400

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from "./screens/screen1"
+import SecondScreen from "./screens/screen2"
+import ThirdScreen from "./screens/screen3"
+import Example1 from "./screens/StateExample"
+import Example2 from "./screens/useEffectExample"
+import StylingExample from "./screens/Styling"
+import FetchExample from "./screens/resourceRetrival"
+import { startClock } from 'react-native-reanimated';
+const Stack = createStackNavigator();
+// This file is the main "entry point" of the app
+// It is the first thing that appears on the string 
+
+// The Stack object is used to move between screens
+// Notice the order of the screen, Home is first therefore it 
+// is the first thing that appears when the app launches
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} initialParams={{test:"test"}} />
+        <Stack.Screen name="SecondScreen" component={SecondScreen} initialParams={{test:"test"}} />
+        <Stack.Screen name="ThirdScreen" component={ThirdScreen} initialParams={{test:"test"}} />
+        <Stack.Screen name="Example1" component={Example1} initialParams={{test:"test"}} />
+        <Stack.Screen name="Example2" component={Example2} initialParams={{test:"test"}} />
+        <Stack.Screen name="Styling" component={StylingExample} initialParams={{test:"test"}} />
+        <Stack.Screen name="Fetch" component={FetchExample} initialParams={{test:"test"}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
